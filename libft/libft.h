ab01c77f8efbedc1d3f0a6e38721a249693f2ec0
Valscr <95771970+Valscr@users.noreply.github.com>
2023-05-29 18:03:51 +0200

    Add files via upload

--- libft/libft.h
+++ libft/libft.h
@@ -31,6 +31,7 @@ int		ft_isprint(unsigned char c);
 int		ft_strlen(const char *str);
 int		ft_toupper(int c);
 int		ft_atoi(const char *str);
+int		ft_atoi_base(char *str, char *base);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_tolower(int c);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
