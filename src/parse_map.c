35a81c99693a81dab432b87f697773cc0792a356
Valscr <95771970+Valscr@users.noreply.github.com>
2023-05-31 18:08:27 +0200

    Add files via upload

--- src/parse_map.c
+++ src/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: valentin <valentin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 13:15:15 by valentin          #+#    #+#             */
-/*   Updated: 2023/05/31 16:17:56 by valentin         ###   ########.fr       */
+/*   Updated: 2023/05/31 18:07:28 by valentin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,92 @@ void	set_pos_player(char c, int y, int x, t_data *game)
 	game->posx = x * 100 + 50;
 
 	return;
+}
+
+int ft_strlen_lesspace(char *str)
+{
+	int i;
+
+	i = ft_strlen(str) - 1;
+	while (i >= 0)
+	{
+		if (str[i] != ' ')
+			return (i);
+		i--;
+	}
+	return (i);
+}
+
+int	check_wall_map(char **str)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (str[0][i])
+	{
+		if (str[0][i] == '0')
+			return (0);
+		i++;
+	}
+	i = 1;
+	while (str[i])
+	{
+		j = 0;
+		while (str[i][j] == ' ')
+			j++;
+		if (str[i][j] != '1')
+			return (0);
+		if (str[i][ft_strlen_lesspace(str[i])] == '0' || ft_strlen_lesspace(str[i]) == 0)
+			return (0);
+		i++;
+	}
+	i--;
+	while (str[i][j])
+	{
+		if (str[i][j] == '0')
+			return (0);
+		j++;
+	}
+	return (1);
+}
+
+int	ft_strlen_less_returnline(char *str)
+{
+	int i;
+
+	i = ft_strlen(str) - 1;
+	while (str[i] == '\n')
+		i--;
+	return (i);
+}
+
+int check_double_return_line(char *str)
+{
+	int i;
+
+	i = ft_strlen_less_returnline(str);
+	while (i > 0)
+	{
+		if (str[i] == 'F' || str[i] == 'C' || (str[i] == 'N' && str[i + 1] == 'O')
+			|| (str[i] == 'S' && str[i + 1] == 'O')|| (str[i] == 'W' && str[i + 1] == 'E')
+				|| (str[i] == 'E' && str[i + 1] == 'A'))
+		{
+			while (str[i] != '\n')
+				i++;
+			while (str[i] != '1')
+				i++;
+			while (i < ft_strlen_less_returnline(str))
+			{
+				if (str[i] == '\n' && str[i + 1] == '\n')
+					return (0);
+				i++;
+			}
+			return (1);
+		}
+		i--;
+	}
+	return (0);
 }
 
 int	copy_map(char **str, t_data *game)
@@ -60,8 +146,10 @@ int	copy_map(char **str, t_data *game)
 		game->map[i] = ft_strdup(str[i]);
 		i++;
 	}
-	if (d != 1)
+	if (d != 1 )
 		return (0);
+	if (!check_wall_map(game->map))
+		return (write(2, "Error : map not close\n", 23), 0);
 	return (1);
 }
 
@@ -106,9 +194,11 @@ int	parse_map(int fd, t_data *game)
 	str = get_next_line(fd);
 	if (str == 0)
 	{
-		write(2, "Empty file\n", 12);
+		write(2, "Error : Empty file\n", 20);
 		exit(0);
 	}
+	if (!check_double_return_line(str))
+		return (write(2, "Error : map not close\n", 23), 0);
 	full = ft_split(str, '\n');
 	free(str);
 	if (!parse_map_texture_color(full, game))
